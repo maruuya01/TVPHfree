392bbde4ba8fcc13f884130e52bf2323cfa736d4
maruuya01 <reusorarobert@gmail.com>
2025-07-18 18:21:09 +0800

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,9 +1,12 @@
+//token.js
+
 export default function handler(req, res) {
   const randomToken = Math.random().toString(36).substring(2, 12);
   const usage = `ott-playback/${randomToken}`;
 
   // ✅ Add CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*'); // or replace * with your frontend domain
+res.setHeader('Access-Control-Allow-Origin', 'https://tvphfree.pages.dev');
+ // or replace * with your frontend domain
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
